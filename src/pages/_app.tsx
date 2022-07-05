a9b6ed7861b1fea66dce4adfcd1d3e93fafd743e
Mateusz Słotwiński <78279510+mateusz-slotwinski@users.noreply.github.com>
2022-07-05 16:05:39 +0200

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import '@mslotwinski/mango-ui'
+import '../styles/index.scss'
+
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
